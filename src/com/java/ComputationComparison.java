a75fd83ebf0a78901a3d764fad17676469a805dc
AswathiMohan23 <achu999here@gmail.com>
2022-10-21 11:07:54 +0530

    [ADDED] : pojo class to enter the co-ordiate details

---
+++ src/com/java/ComputationComparison.java
@@ -0,0 +1,90 @@
+package com.java;
+
+public class ComputationComparison {
+        private int firstX1Coordinate;
+        private int firstX2Coordinate;
+        private int firstY1Coordinate;
+        private int firstY2Coordinate;
+        private int secondX1Coordinate;
+        private int secondX2Coordinate;
+        private int secondY1Coordinate;
+        private int secondY2Coordinate;
+
+        public int getFirstX1Coordinate() {
+                return firstX1Coordinate;
+        }
+
+        public void setFirstX1Coordinate(int firstX1Coordinate) {
+                this.firstX1Coordinate = firstX1Coordinate;
+        }
+
+        public int getFirstX2Coordinate() {
+                return firstX2Coordinate;
+        }
+
+        public void setFirstX2Coordinate(int firstX2Coordinate) {
+                this.firstX2Coordinate = firstX2Coordinate;
+        }
+
+        public int getFirstY1Coordinate() {
+                return firstY1Coordinate;
+        }
+
+        public void setFirstY1Coordinate(int firstY1Coordinate) {
+                this.firstY1Coordinate = firstY1Coordinate;
+        }
+
+        public int getFirstY2Coordinate() {
+                return firstY2Coordinate;
+        }
+
+        public void setFirstY2Coordinate(int firstY2Coordinate) {
+                this.firstY2Coordinate = firstY2Coordinate;
+        }
+
+        public int getSecondX1Coordinate() {
+                return secondX1Coordinate;
+        }
+
+        public void setSecondX1Coordinate(int secondX1Coordinate) {
+                this.secondX1Coordinate = secondX1Coordinate;
+        }
+
+        public int getSecondX2Coordinate() {
+                return secondX2Coordinate;
+        }
+
+        public void setSecondX2Coordinate(int secondX2Coordinate) {
+                this.secondX2Coordinate = secondX2Coordinate;
+        }
+
+        public int getSecondY1Coordinate() {
+                return secondY1Coordinate;
+        }
+
+        public void setSecondY1Coordinate(int secondY1Coordinate) {
+                this.secondY1Coordinate = secondY1Coordinate;
+        }
+
+        public int getSecondY2Coordinate() {
+                return secondY2Coordinate;
+        }
+
+        public void setSecondY2Coordinate(int secondY2Coordinate) {
+                this.secondY2Coordinate = secondY2Coordinate;
+        }
+
+        @Override
+        public String toString() {
+                return "ComputationComparison{" +
+                        "firstX1Coordinate=" + firstX1Coordinate +
+                        ", firstX2Coordinate=" + firstX2Coordinate +
+                        ", firstY1Coordinate=" + firstY1Coordinate +
+                        ", firstY2Coordinate=" + firstY2Coordinate +
+                        ", secondX1Coordinate=" + secondX1Coordinate +
+                        ", secondX2Coordinate=" + secondX2Coordinate +
+                        ", secondY1Coordinate=" + secondY1Coordinate +
+                        ", secondY2Coordinate=" + secondY2Coordinate +
+                        '}';
+        }
+}
